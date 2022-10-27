0bcbea4316aa33d5c43ed18a1af9565609faa49b
shreyashbijlwanOP <83179433+shreyashbijlwanOP@users.noreply.github.com>
2022-10-27 15:27:17 +0530

    Current Note Project

---
+++ Add Notes Project/index2.js
@@ -0,0 +1,10 @@
+// var isIsomorphic = function(s, t) {
+//     for(let i = 0; i < s.length; i++){
+//         const value = s.indexOf(s[i]);
+//         const value2 = t.indexOf(t[i]);
+//         console.log(value,value2) 
+//         if (s[value] !== s[value2] || t[value] !== t[value2]) return false;
+//     }
+//     return true
+// };
+
